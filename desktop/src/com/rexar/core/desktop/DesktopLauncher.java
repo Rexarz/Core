0d4cba73bc0a56e0a0335bb91e79b82bf7ecc773
rexarz@gmail.com <rexarz@gmail.com>
2017-05-25 23:34:29 +0300

    Added base project

---
+++ desktop/src/com/rexar/core/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.rexar.core.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rexar.core.CoreGame;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.height = 640;
+        config.width = config.width * 16 / 9;
+
+        new LwjglApplication(new CoreGame(), config);
+    }
+}
